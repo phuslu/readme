28a9670f744e56a4b8e7e83b73f2b9cd247c4bd8
phuslu <phuslu@hotmail.com>
2019-03-27 20:24:59 +0800

    hide lab.phus.lu

--- tz.php
+++ tz.php
@@ -520,7 +520,7 @@ body {
 <div class="container">
 
 <table>
-<?php if ($_SERVER['HTTP_HOST'] == 'lab.phus.lu'): ?>
+<?php if ($_SERVER['HTTP_HOST'] == 'phus.lu'): ?>
 	<tr>
 	<th><a href="?method=phpinfo">PHP Info</a></th>
 	<th><a href="/files/"><?php __('Download'); ?></a></th>
