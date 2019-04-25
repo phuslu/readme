e2aa532ed1f87c2c2cd8a1fb938e09b1b321812d
phuslu <phuslu@hotmail.com>
2019-04-25 17:29:53 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -3,19 +3,19 @@
 set -ex
 
 AUTOINDEX_HTML=$(pwd -P)/autoindex.html
-for DIR in . $(ls -l | grep '^d' | awk '{print $NF}'); do
-    pushd ${DIR}
-    ls -phl --time-style='+%d-%b-%Y %H:%M' | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | sort -k1,1 -k8,8 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)} END{print "</pre></body></html>"}' >index.html
-    cat ${AUTOINDEX_HTML} >>index.html
-    popd
-done
 
 for FILE in $(find . -type f -name "*.md" -not -iname "readme.md"); do
 	cat ${FILE} markdown.html >${FILE%.*}.html
 	git add ${FILE%.*}.html
 done
 
-find . -type f -name "index.html" -exec git add "{}" \;
+for DIR in . $(ls -l | grep '^d' | awk '{print $NF}'); do
+    pushd ${DIR}
+    ls -phl --time-style='+%d-%b-%Y %H:%M' | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | sort -k1,1 -k8,8 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)} END{print "</pre></body></html>"}' >index.html
+    cat ${AUTOINDEX_HTML} >>index.html
+	git add index.html
+    popd
+done
 
 git commit -m "[skip ci] build new index" -s || true
 
