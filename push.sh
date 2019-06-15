20b13bcfc076bddb087341e00b0eed5489367310
phuslu <phuslu@hotmail.com>
2019-06-15 22:03:29 +0800

    test

--- push.sh
+++ push.sh
@@ -11,9 +11,9 @@ done
 
 for DIR in . $(ls -l | grep '^d' | awk '{print $NF}'); do
     pushd ${DIR}
-    ls -phl --time-style='+%d-%b-%Y %H:%M' | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | sort -k1,1 -k8,8 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)} END{print "</pre></body></html>"}' >index.html
-    cat ${AUTOINDEX_HTML} >>index.html
-	git add index.html
+    ls -phl --time-style='+%d-%b-%Y %H:%M' --group-directories-first | awk 'BEGIN{print "<title>Index of /</title>"}{printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)}' >index.html
+    cat /opt/home/phuslu.github.io/autoindex.html >>index.html
+    git add index.html
     popd
 done
 
