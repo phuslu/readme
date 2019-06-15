2c878b08267885ebb00caa1fb508a4a4137d6d15
phuslu <phuslu@hotmail.com>
2019-06-15 23:31:30 +0800

    tweak autoindex.sh

--- autoindex.sh
+++ autoindex.sh
@@ -5,18 +5,21 @@ set -ex
 ROOT="$(cd "$(dirname "$0")"; pwd -P)"
 
 function index() {
-	ls -phl --time-style='+%d-%b-%Y %H:%M' --ignore index.html --group-directories-first |
-	awk 'BEGIN {print "<title>Index of /</title>"}
-		{printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)}' >index.html
-	cat ${ROOT}/autoindex.html >>index.html
+	(
+		echo '<title>Index of /</title>'
+		find . -maxdepth 1 -type d -not -path '*/\.*' -not -path . -printf '<a href="%f/">%f/</a> %Td-%Tb-%TY %TH:%TM %s\n'
+		find . -maxdepth 1 -type f -printf '<a href="%f">%f</a> %Td-%Tb-%TY %TH:%TM %s\n'
+		cat ${ROOT}/autoindex.html
+	) >index.html
 }
 
 function gen() {
-	find . -type d -not -path '*/\.*' -exec bash -c "cd {} && $ROOT/$0 index && git add index.html" \;
+	find . -type d -not -path '*/\.*' -exec bash -c "cd {} && $ROOT/$0 index" \;
 }
 
 function push() {
 	bash $0 gen
+	find . -type d -not -path '*/\.*' -exec bash -c "git add {}/index.html" \;
 	git commit -m "[skip ci] build new index" -s -a || true
 	git push origin master
 }
