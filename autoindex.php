dbe20d0dc40fbdf43464861ad61d9b5a38e9f097
phuslu <phuslu@hotmail.com>
2019-07-06 12:30:25 +0800

    [skip ci] build new index
    Signed-off-by: phuslu <phuslu@hotmail.com>

--- autoindex.php
+++ autoindex.php
@@ -1,17 +1,27 @@
 <?php
+/*
+autoindex.php for apache
+
+.htaccess
+
+  AddDefaultCharset utf-8
+  RewriteEngine on
+  RewriteRule ^.+/$ autoindex.php
+  RewriteRule ^$ autoindex.php
+
+*/
 
 $rootdir = $_SERVER['DOCUMENT_ROOT'];
 $dirname = $_SERVER['REQUEST_URI'];
 
-function human_filemtime($file) {
-    return date('j-M-Y H:i', filemtime($GLOBALS['rootdir'] . $GLOBALS['dirname'] . $file));
-}
-
-function human_filesize($file) {
-    $size = filesize($GLOBALS['rootdir'] . $GLOBALS['dirname'] . $file);
-    $factor = floor(log($size, 1024));
+function human_fileinfo($file) {
+    $fullname = $GLOBALS['rootdir'] . $GLOBALS['dirname'] . $file;
+    $bytes = filesize($fullname);
+    $factor = floor(log($bytes, 1024));
     $units = array('', 'KB', 'MB', 'GB', 'TB', 'PB');
-    return sprintf("%.0f%s", $size / pow(1024, $factor), $units[$factor]);
+    $mtime = date('j-M-Y H:i', filemtime($fullname));
+    $size = sprintf("%.0f%s", $bytes / pow(1024, $factor), $units[$factor]);
+    return $mtime . ' ' . $size;
 }
 
 $files = array();
@@ -22,10 +32,10 @@ foreach (scandir($rootdir . $dirname) as $file)
 printf("<meta charset='UTF-8'><title>Index of %s</title><hr><pre>\n", $dirname);
 foreach ($files as $file)
     if (is_dir($file))
-        printf("<a href='%s/'>%s/</a> %s %s\n", $file, $file, human_filemtime($file), human_filesize($file));
+        printf("<a href='%s/'>%s/</a> %s\n", $file, $file, human_fileinfo($file));
 foreach ($files as $file)
     if (!is_dir($file))
-        printf("<a href='%s'>%s</a> %s %s\n", $file, $file, human_filemtime($file), human_filesize($file));
+        printf("<a href='%s'>%s</a> %s\n", $file, $file, human_fileinfo($file));
 printf("</pre><hr>\n");
 
 readfile($rootdir . '/autoindex.html');
