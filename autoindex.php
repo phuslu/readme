c5cf8b79e92bb661b86ce61ac87ed5368584b3c6
phuslu <phuslu@hotmail.com>
2019-07-06 16:17:25 +0800

    tweak autoindex.php

--- autoindex.php
+++ autoindex.php
@@ -15,27 +15,27 @@ $rootdir = $_SERVER['DOCUMENT_ROOT'];
 $dirname = $_SERVER['REQUEST_URI'];
 
 function human_fileinfo($file) {
-    $fullname = $GLOBALS['rootdir'] . $GLOBALS['dirname'] . $file;
-    $bytes = filesize($fullname);
-    $factor = floor(log($bytes, 1024));
-    $units = array('', 'KB', 'MB', 'GB', 'TB', 'PB');
-    $mtime = date('j-M-Y H:i', filemtime($fullname));
-    $size = sprintf("%.0f%s", $bytes / pow(1024, $factor), $units[$factor]);
-    return $mtime . ' ' . $size;
+	$fullname = $GLOBALS['rootdir'] . $GLOBALS['dirname'] . $file;
+	$mtime = date('j-M-Y H:i', filemtime($fullname));
+	$bytes = filesize($fullname);
+	$factor = floor(log($bytes, 1024));
+	$units = array('', 'KB', 'MB', 'GB', 'TB', 'PB');
+	$size = sprintf("%.0f%s", $bytes / pow(1024, $factor), $units[$factor]);
+	return $mtime . ' ' . $size;
 }
 
 $files = array();
 foreach (scandir($rootdir . $dirname) as $file)
-    if ($file[0] != '.' && $file[0] != '@' && $file != 'autoindex.php')
-        array_push($files, $file);
+	if ($file[0] != '.' && $file[0] != '@' && $file != 'autoindex.php')
+		array_push($files, $file);
 
 printf("<meta charset='UTF-8'><title>Index of %s</title><hr><pre>\n", $dirname);
 foreach ($files as $file)
-    if (is_dir($file))
-        printf("<a href='%s/'>%s/</a> %s\n", $file, $file, human_fileinfo($file));
+	if (is_dir($file))
+		printf("<a href='%s/'>%s/</a> %s\n", $file, $file, human_fileinfo($file));
 foreach ($files as $file)
-    if (!is_dir($file))
-        printf("<a href='%s'>%s</a> %s\n", $file, $file, human_fileinfo($file));
+	if (!is_dir($file))
+		printf("<a href='%s'>%s</a> %s\n", $file, $file, human_fileinfo($file));
 printf("</pre><hr>\n");
 
 readfile($rootdir . '/autoindex.html');
